7cfa488269d6a05eff06246770c2ba337ab5b084
Caelan Neumann <caelann2@gmail.com>
2021-08-19 22:12:38 -0600

    Declare methods

--- Critereon.java
+++ Critereon.java
@@ -1,15 +1,22 @@
 
 /**
- * Critereon - Write a description here.
+ * Critereon - Critereon Class for Decision matrix.
  * 
- * @author (your name) 
+ * @author Caelan Neumann and Kevin Bai 
  * @version (a version number or a date)
  */
 // Standard import for the Scanner class
 import java.util.*;
 public class Critereon {
-    String Criteon;
-    String grade;
-    
+    private String Criteon;
+    private String grade;
+
+    public void newCritereon() {
+
+        
+    }
+    public void setGrade() {
+        
+    }    
     
 }
