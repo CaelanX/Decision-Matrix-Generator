8040004f1e492dd2d37fa43e380708c09edc55a9
Kevin Bai <84043195+Pwumpkin@users.noreply.github.com>
2021-08-19 21:33:07 -0700

    Defined Alternative
    -created the fields for Alternatives

---
+++ Alternatives.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Alternatives
+{
+    // instance variables - replace the example below with your own
+    String model;
+    ArrayList <Critereon> attributes;
+    
+    public Alternatives(String m, ArrayList<Critereon> a){
+        model = m;
+        attributes = a;
+    }
+    
+    public ArrayList<Critereon> getAttributes(){
+        return attributes;
+    }
+    
+    public static boolean equals(Alternatives a1, Alternatives a2){
+        boolean sameSize = false;
+        boolean 
+        
+        
+        return 
+    }
+}
