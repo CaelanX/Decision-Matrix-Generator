ddeb4632da9c9896e40c16a7656524d8bcd002a6
Caelan Neumann <caelann2@gmail.com>
2021-08-19 22:04:32 -0600

    Create Critereon

---
+++ Critereon.java
@@ -0,0 +1,15 @@
+
+/**
+ * Critereon - Write a description here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+// Standard import for the Scanner class
+import java.util.*;
+public class Critereon {
+    String Criteon;
+    String grade;
+    
+    
+}
